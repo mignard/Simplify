43c16df6561586d614fd9ba945afaa1070769575
Alexanderius <alexander.krylkov@gmail.com>
2014-04-03 16:53:13 +0600

    Initial classes added

---
+++ Simplify.Web.Mvc/LocalizableRazorView.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Web.Mvc;
+
+namespace Simplify.Web.Mvc
+{
+	/// <summary>
+	/// Localizable razor engine view
+	/// </summary>
+	public class LocalizableRazorView : RazorView
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="LocalizableRazorView"/> class.
+		/// </summary>
+		/// <param name="controllerContext">The controller context.</param>
+		/// <param name="viewPath">The view path.</param>
+		/// <param name="layoutPath">The layout or master page.</param>
+		/// <param name="runViewStartPages">A value that indicates whether view start files should be executed before the view.</param>
+		/// <param name="viewStartFileExtensions">The set of extensions that will be used when looking up view start files.</param>
+		public LocalizableRazorView(ControllerContext controllerContext, string viewPath, string layoutPath, bool runViewStartPages, IEnumerable<string> viewStartFileExtensions) : base(controllerContext, viewPath, layoutPath, runViewStartPages, viewStartFileExtensions)
+		{
+			LoadLocalizationFiles();
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="LocalizableRazorView"/> class.
+		/// </summary>
+		/// <param name="controllerContext">The controller context.</param>
+		/// <param name="viewPath">The view path.</param>
+		/// <param name="layoutPath">The layout or master page.</param>
+		/// <param name="runViewStartPages">A value that indicates whether view start files should be executed before the view.</param>
+		/// <param name="viewStartFileExtensions">The set of extensions that will be used when looking up view start files.</param>
+		/// <param name="viewPageActivator">The view page activator.</param>
+		public LocalizableRazorView(ControllerContext controllerContext, string viewPath, string layoutPath, bool runViewStartPages, IEnumerable<string> viewStartFileExtensions, IViewPageActivator viewPageActivator)
+			: base(controllerContext, viewPath, layoutPath, runViewStartPages, viewStartFileExtensions, viewPageActivator)
+		{
+			LoadLocalizationFiles();
+		}
+
+		private void LoadLocalizationFiles()
+		{		
+		}
+
+		/// <summary>
+		/// Renders the specified view context by using the specified the writer object.
+		/// </summary>
+		/// <param name="viewContext">Information related to rendering a view, such as view data, temporary data, and form context.</param>
+		/// <param name="writer">The writer object.</param>
+		public override void Render(ViewContext viewContext, TextWriter writer)
+		{
+			base.Render(viewContext, writer);
+		}		 
+	}
+}
---
+++ Simplify.Web.Mvc/LocalizableRazorViewEngine.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace Simplify.Web.Mvc
+{
+	/// <summary>
+	/// Localizable razor views engine
+	/// </summary>
+	public class LocalizableRazorViewEngine : RazorViewEngine
+	{
+		/// <summary>
+		/// Creates a view by using the specified controller context and the paths of the view and master view.
+		/// </summary>
+		/// <param name="controllerContext">The controller context.</param>
+		/// <param name="viewPath">The path to the view.</param>
+		/// <param name="masterPath">The path to the master view.</param>
+		/// <returns>
+		/// The view.
+		/// </returns>
+		protected override IView CreateView(ControllerContext controllerContext, string viewPath, string masterPath)
+		{
+			var view = new LocalizableRazorView(controllerContext, viewPath,
+				masterPath, true, FileExtensions, ViewPageActivator);
+
+			return view;
+		}
+	}
+}
